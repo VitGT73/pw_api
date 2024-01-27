7d25d57a6fde296abfa69489ed1a96798c35cdb6
VitGT <tvit@mail.ru>
2024-01-27 16:42:11 +0500

    del comments

--- lib/fixtures/fixtures.ts
+++ lib/fixtures/fixtures.ts
@@ -3,12 +3,6 @@ import { expect as toBeOneOfValuesExpect } from "@fixtures/toBeOneOfValues";
 import { expect as toBeValidDate } from "@fixtures/toBeValidDate";
 import { expect as typesExpects } from "@fixtures/typesExpects";
 
-// declare global {
-//   interface expect {
-//     toBeValidDate(received: any): this;
-//   }
-// }
-
 export { test } from "@playwright/test";
 
 export const expect = mergeExpects(toBeOneOfValuesExpect, toBeValidDate, typesExpects);
